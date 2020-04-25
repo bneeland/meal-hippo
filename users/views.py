c00a5ced79626aea3dd9544e961c3e4633e357e8
Brian Neeland <brian.neeland@protonmail.com>
2020-04-25 07:29:09 -0600

    Added automatic login and redirect to order items page

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.contrib.auth import authenticate, login
 
 from .forms import CustomUserCreationForm
 
@@ -8,3 +9,17 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        valid = super(SignUpView, self).form_valid(form)
+        email, password = form.cleaned_data.get('email'), form.cleaned_data.get('password1')
+        new_user = authenticate(email=email, password=password)
+        login(self.request, new_user)
+        return valid
+
+    def get_success_url(self):
+        next = self.request.POST.get('next')
+        if next == None:
+            return reverse_lazy('webplatform:home_view')
+        else:
+            return next
