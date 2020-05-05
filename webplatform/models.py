4a77e873ef1350ee53450b7d1fd7d913164732c0
Brian Neeland <brian.neeland@protonmail.com>
2020-05-05 17:10:20 -0600

    Tweaks based on test run

--- webplatform/models.py
+++ webplatform/models.py
@@ -81,7 +81,7 @@ class UserDeliveryDetail(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     phone = models.CharField(max_length=20)
     address = models.TextField()
-    instructions = models.TextField()
+    instructions = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.user.email
