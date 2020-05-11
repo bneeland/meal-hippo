083f9430019bde3f661b3a94b4810bcf7a78c912
Brian Neeland <brian.neeland@protonmail.com>
2020-05-10 23:05:16 -0600

    Added tasks to users app

---
+++ users/tasks.py
@@ -0,0 +1,14 @@
+from django.core.mail import send_mail
+from celery import shared_task
+from django.utils import timezone
+
+@shared_task
+def send_mail_with_celery(subject, message):
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email='web.bot@mealhippo.com',
+        recipient_list=['hello@mealhippo.com'],
+        fail_silently=True,
+    )
+    return None
