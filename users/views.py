e264fe5263957ca00f196d8d426b23e1a31f40b0
Brian Neeland <brian.neeland@protonmail.com>
2020-05-22 17:55:53 -0600

    Added a welcome email

--- users/views.py
+++ users/views.py
@@ -25,6 +25,13 @@ class SignUpView(CreateView):
             html_message='<h1>New user</h1><p>A new user signed up on mealhippo.com beta.</p><p>The user who did this was '+email+'.</p><p>This was done at '+str(timezone.localtime(timezone.now()))+'.</p>',
         )
 
+        tasks.send_mail_with_celery.delay(
+            subject='Thanks for signing up to Meal Hippo!',
+            message='Hi '+email+', Thanks for signing up on mealhippo.com! If you have any questions about Meal Hippo, such as how to start your order, don\'t hesitate to get in touch with me by phone or email. I\'d be happy to help. -Brian from Meal Hippo | Call: 780-243-7675 | Email: hello@mealhippo.com | beta.mealhippo.com',
+            recipient_list=[email],
+            html_message='<h1>Thanks for signing up to Meal Hippo!</h1><p>Hi '+email+',</p><p>Thanks for signing up on mealhippo.com!</p><p>If you have any questions about Meal Hippo, such as how to start your order, don\'t hesitate to get in touch with me by phone or email. I\'d be happy to help.</p><p>Brian</p><p>Meal Hippo<br>Meal Hippo<br>Call: 780-243-7675<br>Email: hello@mealhippo.com<br><a href="https://b.mealhippo.com">beta.mealhippo.com</a></p>',
+        )
+
         return valid
 
     def get_success_url(self):
